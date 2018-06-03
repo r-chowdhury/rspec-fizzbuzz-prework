60f0d33e0ef5d920c0e60f15f5dd2aac47860fc4
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-06-03 01:56:38 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+    return "FizzBuzz"
+  if num % 3 == 0
+    return "Fizz"
+  if num % 5 == 0
+    return "Buzz"
+  else
+    return nil
+  end
+end
